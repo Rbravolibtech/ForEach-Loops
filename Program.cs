56951247b756b1738e4e00076ac58421e0b66aee
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-12-11 15:12:25 -0700

    coded used foreach loops

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ForEach_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[] { 1, 2, 3, 4 };
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            // Uncomment the following code to see the iteration over characters in a string
+
+            // var name = "Rock Cena";
+            // for (var i = 0; i < name.Length; i++)
+            // {
+            //     Console.WriteLine(name[i]);
+            // }
+
+            // FOREACH LOOP
+            // foreach (var character in name)
+            // {
+            //     Console.WriteLine(character);
+            // }
+        }
+    }
+}
